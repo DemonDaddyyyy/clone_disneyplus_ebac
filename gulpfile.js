961700848a6757313c2efb8ccbab3c6d6ac10b1a
Demon_Daddyyyy <musicnewstube@gmail.com>
2026-07-18 01:16:38 +0100

    the end

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
+const uglify = require ('gulp-uglify');
+
+function scripts() {
+  return gulp.src('./src/scripts/*.js')   // caminho corrigido
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
 return gulp.src('./src/styles/*.scss')
@@ -21,4 +28,4 @@ gulp.watch('./src/styles/*.scss', styles);
 
 exports.sass = styles;
 exports.watch = watchFiles;
-exports.default = gulp.parallel(styles, images, watchFiles);
+exports.default = gulp.parallel(styles, images, watchFiles, scripts);
