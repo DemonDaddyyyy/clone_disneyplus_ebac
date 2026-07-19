45c09fec384007d4257315c272b112d833b61756
Demon_Daddyyyy <musicnewstube@gmail.com>
2026-07-19 12:07:16 +0100

    alterações p ver se o deploy finalmente funciona

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,38 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
-const uglify = require ('gulp-uglify');
+const uglify = require('gulp-uglify');
 
 function scripts() {
-  return gulp.src('./src/scripts/*.js')   // caminho corrigido
+  return gulp.src('./src/scripts/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulp.dest('./dist/js'));
 }
 
 function styles() {
-return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ style: 'compressed' }))
+  return gulp.src('./src/styles/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
-return gulp.src('./src/images/**/*')
+  return gulp.src('./src/images/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./dist/images'));
 }
 
 function watchFiles() {
-gulp.watch('./src/styles/*.scss', styles);
+  gulp.watch('./src/styles/*.scss', styles);
+  gulp.watch('./src/scripts/*.js', scripts);
 }
 
+const build = gulp.parallel(styles, images, scripts); // SEM watch — termina normalmente
+
 exports.sass = styles;
+exports.scripts = scripts;
+exports.images = images;
 exports.watch = watchFiles;
-exports.default = gulp.parallel(styles, images, watchFiles, scripts);
+exports.build = build;                  
+exports.default = gulp.series(build, watchFiles);
